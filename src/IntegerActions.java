2a885ad9571469892f12b766744c06ae3581e26f
Jannis Lauerwald <jannis1lauerwald@gmail.com>
2023-09-15 23:22:43 +0200

    Added Simple Mathematics-functions

---
+++ src/IntegerActions.java
@@ -0,0 +1,21 @@
+public class IntegerActions {
+    private int ergebnis;
+
+    public void add(int firstNumber, int secondNumber) {
+        this.ergebnis = firstNumber + secondNumber;
+    }
+
+    public void sub(int firstNumber, int secondNumber) {
+        this.ergebnis = firstNumber - secondNumber;
+    }
+
+    public void multiply(int firstNumber, int secondNumber) {
+        this.ergebnis = firstNumber * secondNumber;
+    }
+
+    public void div(int firstNumber, int secondNumber) {
+        this.ergebnis = firstNumber / secondNumber;
+    }
+
+
+}
