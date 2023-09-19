e1011bec27e6db3ac48b5585283eae517cefd09f
Jannis Lauerwald <jannis1lauerwald@gmail.com>
2023-09-19 19:28:57 +0200

    Added power methode

--- src/IntegerActions.java
+++ src/IntegerActions.java
@@ -17,5 +17,27 @@ public class IntegerActions {
         this.ergebnis = firstNumber / secondNumber;
     }
 
+    public void power(int basis, int exponent) {
+        int result = 0;
 
+        if (exponent == 0) {
+            result = 1;
+        } else if (exponent == 1) {
+            result = basis;
+        } else {
+            for (int i = 0; i <= exponent; i++) {
+                if (i == 2) {
+                    result = basis;
+                }
+                result *= basis;
+            }
+        }
+        this.ergebnis = result;
+
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(ergebnis);
+    }
 }
--- src/main.java
+++ src/main.java
@@ -1,6 +1,12 @@
 public class main {
     public static void main(String[] args) {
-        System.out.println("Test");
+        IntegerActions result1 = new IntegerActions();
+
+        for (int i = 0; i <= 11; i++) {
+            result1.power(2, i);
+            System.out.println(result1.toString());
+        }
+
 
     }
 }
